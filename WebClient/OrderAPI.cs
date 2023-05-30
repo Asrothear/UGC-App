130a2e513a37b8fe2345de367216ad3d63874ac0
Lord Asrothear <lord@asrothear.de>
2023-05-31 01:05:06 +0200

    Create OrderAPI.cs
    WebClient specefic to get Order relevant Data from API

---
+++ WebClient/OrderAPI.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using UGC_App.Order.DashViews;
+
+namespace UGC_App.WebClient;
+
+public class OrderAPI
+{
+    internal static dynamic GetSystemList()
+    {
+        var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, Config.Instance.SystemDataUrl);
+        var response = client.Send(request);
+        
+        dynamic? content = null;
+        
+        if (response.IsSuccessStatusCode)
+        {
+            content = response.Content.ReadFromJsonAsync<HashSet<dynamic>>().Result;
+        }
+        return  content;
+    }
+    internal static dynamic GetSystemHistory(string address)
+    {
+        var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{Config.Instance.SystemDataUrl}/{address}");
+        var response = client.Send(request);
+        
+        dynamic? content = null;
+        
+        if (response.IsSuccessStatusCode)
+        {
+            try
+            {
+                content = response.Content.ReadFromJsonAsync<dynamic>().Result;
+            }
+            catch 
+            {
+                content = response.Content.ReadAsStringAsync().Result;
+            }
+        }
+        return  content;
+    }
+}
